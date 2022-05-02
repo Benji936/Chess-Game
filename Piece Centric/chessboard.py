a99c9ea84376175784a0d7ae1bd1ad505503e905
benjamin.sitbon <benjamin.sitbon@etu.hesge.ch>
2022-05-03 00:33:11 +0200

    separated display from the chessboard

--- Piece Centric/chessboard.py
+++ Piece Centric/chessboard.py
@@ -1,35 +1,11 @@
 import pygame
 import copy
-from king import King
-from queen import Queen
-from bishop import Bishop
-from rook import Rook
-from knight import Knight
-from pawn import Pawn
 
 
-pieces = {"k" : King(0,0,"white"), "K" : King(0,0,"black"), "q" : Queen(0,0,"white"), "Q" : Queen(0,0,"black"), "b" : Bishop(0,0,"white"), 
-"B":Bishop(0,0,"black"), "r" : Rook(0,0,"white"), "R" : Rook(0,0,"black"), "n" : Knight(0,0,"white"), "N" : Knight(0,0,"black"), 
-"p" : Pawn(0,0,"white"), "P" : Pawn(0,0,"black")}
+
+
 
 class Chessboard:
-
-    def convertStringInBoard(self,string):
-        x = 0
-        y = 0
-        self.pieces = []
-        for char in string:
-            try:
-                x += int(char)
-            except:
-                if char == "/":
-                    y += 1
-                    x = 0
-                else:
-                    pieces[char].x = x
-                    pieces[char].y = y
-                    self.pieces.append(copy.copy(pieces[char]))
-                    x+=1
         
     def getPiece(self,x,y):
         for piece in self.pieces:
@@ -38,16 +14,13 @@ class Chessboard:
         return 0
 
 
-    def __init__(self,scale,start):
+    def __init__(self):
         #Peut être plutot utilisé un Set qu'une liste
         #Ou alors un dictionnaire avec l'indice du tableau
         self.pieces = []
-        self.scale = scale 
-        self.convertStringInBoard(start)
+
     
-    def display(self,screen):
-        for piece in self.pieces:
-            screen.blit(piece.image,(piece.x*self.scale,piece.y*self.scale))
+
 
 
 
--- Piece Centric/game.py
+++ Piece Centric/game.py
@@ -1,7 +1,13 @@
+from king import King
+from queen import Queen
 from bishop import Bishop
+from rook import Rook
+from knight import Knight
+from pawn import Pawn
 from piece import Piece
 from chessboard import Chessboard
 
+import copy
 import sys, pygame
 from pygame.locals import *
 
@@ -13,6 +19,10 @@ scale = 800
 size = scale, scale
 cellScale = int(scale/8)
 
+pieces = {"k" : King(0,0,"white"), "K" : King(0,0,"black"), "q" : Queen(0,0,"white"), "Q" : Queen(0,0,"black"), "b" : Bishop(0,0,"white"), 
+"B":Bishop(0,0,"black"), "r" : Rook(0,0,"white"), "R" : Rook(0,0,"black"), "n" : Knight(0,0,"white"), "N" : Knight(0,0,"black"), 
+"p" : Pawn(0,0,"white"), "P" : Pawn(0,0,"black")}
+
 
 pygame.init()
 screen = pygame.display.set_mode(size)
@@ -21,9 +31,9 @@ screen = pygame.display.set_mode(size)
 def isWhite(x,y):
     return (x%2 + y%2)%2 == 0
 
-def loadImages(board):
+def loadImages(board,scale):
     for piece in board.pieces:
-        piece.loadImage(board.scale)
+        piece.loadImage(scale)
 
 def drawSquare(x,y,scale,w,b):
     cell = pygame.Rect(x*scale,y*scale,scale,scale)
@@ -56,48 +66,74 @@ def  drawBoard():
             yCount += cellScale
 
 
+def display(screen,scale,pieces):
+    for piece in pieces:
+        screen.blit(piece.image,(piece.x*scale,piece.y*scale))
+
+def convertStringInBoard(board,string):
+        x = 0
+        y = 0
+        board.pieces = []
+        for char in string:
+            try:
+                x += int(char)
+            except:
+                if char == "/":
+                    y += 1
+                    x = 0
+                else:
+                    pieces[char].x = x
+                    pieces[char].y = y
+                    board.pieces.append(copy.copy(pieces[char]))
+                    x+=1
 
 
 def movePiece(piece,x,y):
     drawSquare(piece.x,piece.y,cellScale,white,black)
     if(piece.move(x,y,game)):
         drawSquare(x,y,cellScale,white,black)
-    piece.display(screen,game.scale)
-
-
-game = Chessboard(cellScale,"RNBKQBNR/PPPPPPPP/////pppppppp/rnbkqbnr")
-loadImages(game)
-drawBoard()
-game.display(screen)
-
-pieceSelected = 0
-select = game.getPiece(0,0)
-
-
-while 1:
-
-    for event in pygame.event.get():
-
-        if event.type == pygame.QUIT: sys.exit()
-
-        elif event.type == pygame.MOUSEBUTTONUP:
-            pos = pygame.mouse.get_pos()
-            pos1 = int(pos[0]/cellScale)
-            pos2 = int(pos[1]/cellScale)
-
-            if pieceSelected:
-                movePiece(select,pos1,pos2)
-                pieceSelected = 0
-            
-                select = None
-            else:
-                select = game.getPiece(pos1,pos2)
-                if select:
-                    drawSquare(pos1,pos2,cellScale,selected_white,selected_black)
-                    select.display(screen,game.scale)
-                    if(select != 0): pieceSelected = 1
+    piece.display(screen,cellScale)
 
 
 
-    pygame.display.flip()
+
+
+if __name__ == "__main__":
+
+
+    game = Chessboard()
+    convertStringInBoard(game,"RNBKQBNR/PPPPPPPP/////pppppppp/rnbkqbnr")
+    loadImages(game,cellScale)
+    drawBoard()
+    display(screen,cellScale,game.pieces)
+
+    pieceSelected = 0
+    select = game.getPiece(0,0)
+
+
+    while 1:
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT: sys.exit()
+
+            elif event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                pos1 = int(pos[0]/cellScale)
+                pos2 = int(pos[1]/cellScale)
+
+                if pieceSelected:
+                    movePiece(select,pos1,pos2)
+                    pieceSelected = 0
+                    select = None
+                else:
+                    select = game.getPiece(pos1,pos2)
+                    if select:
+                        drawSquare(pos1,pos2,cellScale,selected_white,selected_black)
+                        select.display(screen,cellScale)
+                        pieceSelected = 1
+
+
+
+        pygame.display.flip()
     
